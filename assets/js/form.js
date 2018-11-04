688863738c6cc54750c7d174a9b331cc482b8b11
andrea <andrea.londono@ucp.edu.co>
2018-11-03 19:44:24 -0500

    se termina maquetacion final

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,16 +2,20 @@ $(document).ready(function(){
     $('#add').on('click', function(e){
         e.preventDefault();
         year = $('#ano').val();
-        habs = $('#habs').val();
+        ventas = $('#ventas').val();
+        emp = $('#emp').val();
         $('#year-list').append(`
             <li class="list-group-item d-flex justify-content-between lh-condensed">
                 <div>
                 <h6 class="my-0">${year}</h6>
                 </div>
-                <span class="text-muted">${habs}</span>
+                <span class="text-muted">${ventas}</span>
+
+                <span class="text-muted">${emp}</span>
             </li>
         `)
         $('#ano').val('');
-        $('#habs').val('');
+        $('#ventas').val('');
+        $('#emp').val('');
     })
 })
