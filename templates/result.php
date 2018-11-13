1052041bdc04310561e93af2a18017a46e7e50fc
andrea <andrea.londono@ucp.edu.co>
2018-11-12 20:18:22 -0500

    comentando algo de codigo

--- templates/result.php
+++ templates/result.php
@@ -21,6 +21,7 @@
     let list = []
     let data = {}
 
+    // consumiendo datos de la api para la grafica ººº trallendo los datos de las ventas
     $.ajax('../php/chart_api.php')
     .done(function(callback) {
       list.push(['Año', 'Ventas'])
@@ -33,6 +34,7 @@
       })
     })
 
+    //consumiendo la api de las sedes ººº para poder listar los datos de los contenidos 
     let city_keys = []
     city_keys.push({nombre: 'Año', id: 'x'})
     $.ajax('../php/sedes_list.php')
@@ -44,17 +46,20 @@
     })
 
     setTimeout(() => {
+      // recorriendo las llavas que se trajeron de la api
       $.each(city_keys, function(key, value){
         $('#keys').append(`<th scope="col">${value.nombre}</th>`)
       })
 
       let data_result = []
       let aready_user_year = []
+      // recorriendo los elementos de la api que contiene los valores  en la base de datos de llaman registros
       $.each(data ,function(key_first, value_first){
         let self = {}
         let first_item = JSON.parse(value_first)
         let valid = true           
         
+        // para no tener que repetir llaves entonces se almacenan las que ya estan usadas en una lista
         $.each(aready_user_year, function(key, value){
           if (value == first_item.ano){
             valid = false
@@ -62,12 +67,13 @@
         })
         
         if (valid && first_item.ano != ''){
+          // se añaden los datos a un objeto para poder recorrerlo 
           self['ano'] = first_item.ano
           self[city_keys[Number(first_item.id_sede)+1].nombre] = first_item.valor
           
           $.each(data ,function(key_second, value_second){
             let second_item = JSON.parse(value_second)
-            
+            // se valida que el año sea el mismo para poder identificar si este valor se puede adjuanta para tener un valor que sea mas vertical
             if (first_item.ano == second_item.ano){
               self[city_keys[Number(second_item.id_sede)+1].nombre] = second_item.valor
             }
@@ -81,18 +87,22 @@
       })
 
       let html = ''
+      // se recorren los datos de las consultas para poder mostrar las tablas de los datos
       $.each(data_result, function(key_data, value_date){
         let html_content = '<tr>'
+        // se recorren cada una de las llaves que tenga el objeto para poder obtener el contenido de forma vertical
         $.each(Object.keys(value_date), function(key_item, value_item){
 
           let keys_empty = []
+          // se utilisa este ciclo para poder limpiar los valor que sean mayores que el que inicia
           $.each(city_keys, function(key, value){
             if (key > key_item){
               keys_empty.push(value)
               return false;
             }
           })
-
+          
+          // se recorren los valores para poder añadir espacios iniciales como tabulacion
           $.each(keys_empty, function(key, value){
             if (value.nombre == 'Año'){
               value.nombre = 'ano'
@@ -103,7 +113,7 @@
               html_content += `<th></th>`
             }
           })
-
+          // si la llave es igual a 0 significa que es a inicial entonce se procede a añadir un atributo adicional
           if (key_item == 0){
             html_content += `<th scope="row">${value_date[value_item]}</th>`
           }else{
@@ -114,8 +124,90 @@
         html+=html_content
       })
 
+      // este es el que añade el html de a tabla
       $('#body_table').html(html)
+      
+      // se recorren cada un de las llaves de la ciudades para realizar calculos por ciudad
+      $.each(city_keys, function(key_id, value_id){
+        let calc_element = {
+          key: '',
+          range: 0,
+          mid: 0,
+          vare: 0,
+          des: 0,
+          asi: 0
+        }
+        calc_element.key = value_id.nombre
+        // en esta variable se guardan solo los valores numericos que pertenezcan a valores
+        let data_list = []
+        let sum = 0 
+        // este es a funcion que suma todos los datos
+        $.each(data, function(key, value){
+          let item = JSON.parse(value)
+          if (item.id_sede == key_id){
+            data_list.push(item.valor)
+            sum += Number(item.valor)
+          }
+        })
+        // estas son las ecuaciones para calcular los datos
+        calc_element.range = data_list[0] - data_list[data_list.length]
+        calc_element.mid = sum / data_list.length
+        calc_element.var = (sum ** 2 / data_list.length) - ( calc_element.mid ** 2)
+        calc_element.des = Math.sqrt(calc_element.var)
+
+        let asimetry = 0
+        $.each(data_list, function(key, value){
+          asimetry += (Number(value) -  calc_element.mid)
+        })
+
+        calc_element.asi = (asimetry ** 3) / (data_list.length * (calc_element.des**3))
+        // estas son la ecuaciones encargadas de limpiar para que esten en base 2
+        calc_element.range = calc_element.range.toFixed(2)
+        calc_element.mid = calc_element.mid.toFixed(2)
+        calc_element.var = calc_element.var.toFixed(2)
+        calc_element.des = calc_element.des.toFixed(2)
+        calc_element.asi = calc_element.asi.toFixed(2)
+
+        $('#content_ranges').append(get_string_of_calc_element(calc_element))
+      })
+
     }, 1000);
+
+    function get_string_of_calc_element(data){
+      return `<div class="row mt-3"> 
+          <h2>${data.key}</h2>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Dato estadistico</th>
+                <th scope="col">Resultado</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Rango</th>
+                <td>${data.range}</td>
+              </tr>
+              <tr>
+                <th scope="row">Media</th>
+                <td>${data.mid}</td>
+              </tr>
+              <tr>
+                <th scope="row">Variansa</th>
+                <td>${data.vare}</td>
+              </tr>
+              <tr>
+                <th scope="row">Desviacion tipica</th>
+                <td>${data.des}</td>
+              </tr>
+              <tr>
+                <th scope="row">Asimetria</th>
+                <td>${data.asi}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>`
+    }
 
     function drawChart(){
         setTimeout(() => {
@@ -157,87 +249,8 @@
           </tbody>
         </table>
       </div>
-      <div class="row mt-3"> 
-        <h2>Pereira</h2>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Dato estadistico</th>
-              <th scope="col">Resultado</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Rango</th>
-              <td>9</td>
-
-            </tr>
-            <tr>
-              <th scope="row">Media</th>
-              <td>23</td>
-            
-            </tr>
-            <tr>
-              <th scope="row">Desviacion tipica</th>
-              <td>56</td>
-            </tr>
-          </tbody>
-        </table>
+      <div id="content_ranges">
       </div>
-      <div class="row mt-3">
-          <h2>Bogota</h2>
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Dato estadistico</th>
-                <th scope="col">Resultado</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">Rango</th>
-                <td>9</td>
-  
-              </tr>
-              <tr>
-                <th scope="row">Media</th>
-                <td>23</td>
-              
-              </tr>
-              <tr>
-                <th scope="row">Desviacion tipica</th>
-                <td>56</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="row mt-3">
-            <h2>Manizales</h2>
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">Dato estadistico</th>
-                  <th scope="col">Resultado</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">Rango</th>
-                  <td>9</td>
-    
-                </tr>
-                <tr>
-                  <th scope="row">Media</th>
-                  <td>23</td>
-                
-                </tr>
-                <tr>
-                  <th scope="row">Desviacion tipica</th>
-                  <td>56</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
       <footer class="my-5 pt-5 text-muted text-center text-small">
         <p class="mb-1">&copy; Andrea</p>
       </footer>
